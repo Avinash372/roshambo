a475f6bb201d1174c468d58fc9a51c1db6e90f2f
Avinash <avinash.sahu3123@gmail.com>
2022-03-13 01:52:43 +0530

    Comments Added Project Finalized

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
+// Computer side selection
 let computerPlay = () => {
   let options = ["rock", "paper", "scissor"];
   return options[Math.floor(Math.random() * 3)];
 };
 
+// Function to play game one round only
 let playRound = (playerSelection, computerSelection) => {
+  // result variable
   let result;
+  // Game logic codes
   if (playerSelection == computerSelection) {
     return (result = `That's a Draw! you both chose ${playerSelection}`);
   } else if (
@@ -24,9 +28,12 @@ let playRound = (playerSelection, computerSelection) => {
   }
 };
 
+// Game of 5 Rounds
 function game(fiveRound) {
+  // counter variables
   let pcounter = 0;
   let ccounter = 0;
+  // loop to iterate a function through the loop(5 Times)
   for (let i = 1; i < 5; i++) {
     const playerSelection = prompt("choose one rock, paper or scissor?", "");
     const computerSelection = computerPlay();
@@ -41,6 +48,7 @@ function game(fiveRound) {
       ccounter;
     }
   }
+  // counter Logic
   if (pcounter > ccounter) {
     console.log(`You Won This Game With Score: ${pcounter} - ${ccounter}`);
   } else if (pcounter < ccounter) {
